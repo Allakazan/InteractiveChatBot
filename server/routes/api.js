5103fc88f8eb626dd7e44ce42c15909689df29f3
Bruno Marques <brunorizzardi80014@hotmail.com>
2018-02-17 18:19:50 -0200

    Front end changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,9 +7,4 @@ router.get('/', (req, res) => {
     res.send('api works');
 });
 
-router.get('/socket', (req, res) => {
-    //req.connection.remoteAddress
-    res.send('socket works');
-});
-
 module.exports = router;
